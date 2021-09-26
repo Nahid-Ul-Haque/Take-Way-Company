ac36c83277ef9ceef4a992ae3f854a4976b9a666
Nahid ul haque <nahidalhaque849@gmai.com>
2021-09-26 15:54:54 +0600

    Add

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -9,14 +9,14 @@ const Details = (props) => {
     return (
         <div className="col-md-4 g-4">
             <div className="col">
-                <div className="card">
+                <div className="card card-container">
                     <img src={img} style={{ 'height': '200px' }} alt="" />
                     <div className="card-body">
-                        <h5 className="card-title"><small>Name: {name}</small></h5>
-                        <p className="card-text"><small>Brand: {brand}</small></p>
-                        <p className="card-text"><small>Engine: {engin}</small></p>
-                        <p className="card-text"><small>Speed: {speed}</small></p>
-                        <p className="card-text"><small>Price: {Price}</small></p>
+                        <h5 className="card-title text-secondary"><small>Name: {name}</small></h5>
+                        <p className="card-text text-secondary"><small>Brand: {brand}</small></p>
+                        <p className="card-text text-secondary"><small>Engine: {engin}</small></p>
+                        <p className="card-text text-secondary"><small>Speed: {speed}</small></p>
+                        <p className="card-text text-secondary"><small>Price: {Price}</small></p>
                         <button onClick={() => props.handleAddCart(props.car)}
                             className='cart-btn'><i className="fas fa-shopping-cart"></i>Add to cart</button>
                     </div>
